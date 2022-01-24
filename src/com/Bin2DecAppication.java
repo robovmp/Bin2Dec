f79b7e518bc1f1c9dcac768b47f0e9a1e0193e6e
Vinícius Martins Pedro <85643969+robovmp@users.noreply.github.com>
2022-01-23 23:40:56 -0300

    Update Bin2DecAppication.java

--- src/com/Bin2DecAppication.java
+++ src/com/Bin2DecAppication.java
@@ -5,10 +5,9 @@ public class Bin2DecAppication {
 	
 	public static void main( String[] args ) {
 		
-		 String bin = javax.swing.JOptionPane.showInputDialog(
-                 "Informe um número em binário (Exemplo: 010101...): ");
+		 String bin = javax.swing.JOptionPane.showInputDialog( "Informe um número em binário (Exemplo: 010101...): " );
 		 try {
-          		javax.swing.JOptionPane.showMessageDialog( null, bin + " ==> " + String.valueOf( Integer.parseInt(bin, 2) ) );
+          		javax.swing.JOptionPane.showMessageDialog( null, bin + " ==> " + String.valueOf( Integer.parseInt( bin, 2 ) ) );
 		 }
 		 catch ( NumberFormatException e ) {
          		 javax.swing.JOptionPane.showMessageDialog( null, bin + " Este não é um número binário..." );
